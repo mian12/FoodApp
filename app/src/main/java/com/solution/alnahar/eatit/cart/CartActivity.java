8252550b88307ed63fe4776dd6b1213d0b66216a
mian12 <idrees.shahbaz@gmail.com>
2018-07-17 00:10:40 -0700

    device location implemented

--- app/src/main/java/com/solution/alnahar/eatit/cart/CartActivity.java
+++ app/src/main/java/com/solution/alnahar/eatit/cart/CartActivity.java
@@ -1,8 +1,14 @@
 package com.solution.alnahar.eatit.cart;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,7 +29,13 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
@@ -39,6 +51,7 @@ import com.solution.alnahar.eatit.Common.Common;
 import com.solution.alnahar.eatit.HomeActivity;
 import com.solution.alnahar.eatit.Interface.RecyclerItemTouchHelperListener;
 import com.solution.alnahar.eatit.MainActivity;
+
 import com.solution.alnahar.eatit.Model.Order;
 import com.solution.alnahar.eatit.Model.RecyclerItemTouchHelper;
 import com.solution.alnahar.eatit.Model.Request;
@@ -61,7 +74,9 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class CartActivity extends AppCompatActivity implements RecyclerItemTouchHelperListener {
+public class CartActivity extends AppCompatActivity implements RecyclerItemTouchHelperListener,
+        GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
 
 
     FirebaseDatabase database;
@@ -81,6 +96,23 @@ public class CartActivity extends AppCompatActivity implements RecyclerItemTouch
 
     Place shippingAddess;
 
+
+
+    //// current Location of device
+   private LocationRequest mLocationRequest;
+   // public LocationRequest mLocationRequest;
+    private GoogleApiClient  mGoogleApiClient;
+    private Location mLastLocation;
+
+    public static final  int  UPDATE_INTERVAL=5000;
+    public static final  int  FASTEST_INTERVAL=3000;
+    public static final  int  DISPLACEMENT=10;
+
+
+
+    public static final  int  REQUEST_LOCATION_CODE=999;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -93,6 +125,29 @@ public class CartActivity extends AppCompatActivity implements RecyclerItemTouch
 //
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+
+
+        // Runtime permissions
+
+
+
+        if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            // Check permission
+            ActivityCompat.requestPermissions(this,new String[]
+                    {android.Manifest.permission.ACCESS_FINE_LOCATION}
+                    ,REQUEST_LOCATION_CODE);
+        }
+        else {
+
+            if (checkPlayServices())
+            {
+                buildingGoogleApiClient();
+                createLocationRequest();
+            }
+        }
+
 
 
         // init apiservice
@@ -148,6 +203,69 @@ public class CartActivity extends AppCompatActivity implements RecyclerItemTouch
         //Log.e("test","onCreate");
 
 
+    }
+
+
+    private synchronized  void buildingGoogleApiClient() {
+        mGoogleApiClient=new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+
+        mGoogleApiClient.connect();
+
+
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest= LocationRequest.create();
+
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+
+
+    }
+
+
+    //ctrlO
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode)
+        {
+            case  REQUEST_LOCATION_CODE:{
+
+                if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                {
+                    if (checkPlayServices())
+                    {
+                        buildingGoogleApiClient();
+                        createLocationRequest();
+                    }
+
+                }
+            }
+
+        }
+    }
+
+    private boolean checkPlayServices() {
+
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int status = googleApiAvailability.isGooglePlayServicesAvailable(this);
+        if(status != ConnectionResult.SUCCESS) {
+            if(googleApiAvailability.isUserResolvableError(status)) {
+                googleApiAvailability.getErrorDialog(this, status, 2404).show();
+                Toast.makeText(this, "This device  is not supported", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 
 
@@ -431,5 +549,73 @@ public class CartActivity extends AppCompatActivity implements RecyclerItemTouch
 
 
         }
+    }
+
+
+
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        displayLocation();
+        startLocationUpdates();
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+
+    private void displayLocation()
+    {
+
+        if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+        mLastLocation=LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        
+        if (mLastLocation!=null)
+        {
+            Log.e("Loction","Your Location"+mLastLocation.getLatitude()+", "+mLastLocation.getLongitude());
+
+        }
+        else
+        {
+            Log.e("Loction","Coudn't get your location!!");
+        }
+
+    }
+
+
+
+    private void startLocationUpdates() {
+
+        if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest,this);
+    }
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation=location;
+        displayLocation();
+
     }
 }
